af357d21dd61e5aff3c67e25301b4788bdf1f9b4
FantaTroll11 <fabian.staempfli@gmx.ch>
2023-04-19 20:29:23 +0200

    -latex MCTS update

--- src/MCTS.h
+++ src/MCTS.h
@@ -6,6 +6,7 @@
 #include <functional>
 
 using namespace BinarySearchTree;
-float gen_random_float(float min, float max);
-node* accessNext(node* Parent);
-int nodetest(node* Parent, int Value, int MaxDepth);
+	float gen_random_float(float min, float max);
+	node* accessNext(node* Parent);
+	int nodetest(node* Parent, int Value, int MaxDepth);
+	int MCTS(int Value, int MaxDepth);
